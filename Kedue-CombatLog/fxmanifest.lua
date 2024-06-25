a2a92463d2c8ad64638d56ce59f1763e0a50edcf
Keduei <moonrptr@gmail.com>
2024-06-25 04:52:36 +0300

    Add files via upload

---
+++ Kedue-CombatLog/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kedue'
+description 'Moon Combatlog'
+version '1.0.0'
+
+client_script "client.lua"
+server_script "server.lua"
+ui_page 'nui/index.html'
+
+files { 
+    'nui/index.html',
+    'nui/script.js'
+}
